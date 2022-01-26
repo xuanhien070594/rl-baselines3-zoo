412808887682928fce895fa525afa76879657110
xuanhien070594@gmail.com <xuanhien070594@gmail.com>
2022-01-26 13:10:52 -0500

    bring back tensorboard sync with wandb

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem-per-gpu=10G
 #SBATCH --cpus-per-gpu=4
 #SBATCH --gpus=1
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --qos=low
 #SBATCH --partition=compute
 
 source /scratch/hien/venvs/rl_contacts_env/bin/activate
 cd /scratch/hien/rl-baselines3-zoo
-python train.py --algo td3 --env CartPole-Softwalls-v1 -n 500000
+python train.py --algo td3 --env CartPole-Softwalls-v1 -n 500000 --tensorboard-log /scratch/hien/tensorboard_log
