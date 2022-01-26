24443298bf3c9129198b1b2ec5b1ce9f90d61f6a
xuanhien070594@gmail.com <xuanhien070594@gmail.com>
2022-01-26 11:12:54 -0500

    add bash script for training on GRASP cluster

---
+++ run_experiment.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --mem-per-gpu=10G
+#SBATCH --cpus-per-gpu=4
+#SBATCH --gpus=1
+#SBATCH --time=1:00:00
+#SBATCH --qos=low
+#SBATCH --partition=compute
+
+source /scratch/hien/venvs/rl_contacts_env/bin/activate
+cd /scratch/hien/rl-baselines3-zoo
+python train.py --algo td3 --env CartPole-Softwalls-v1 -n 500000
