7cdffafe94334e13c867050128029145e231dcd4
Xuan Hien Bui <xuanhien@kostas-ap.seas.upenn.edu>
2022-01-27 15:28:03 -0500

    cluster shell script: change compute node to DAIR computers

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem-per-gpu=10G
 #SBATCH --cpus-per-gpu=4
 #SBATCH --gpus=1
-#SBATCH --time=8:00:00
-#SBATCH --partition=compute
+#SBATCH --time=24:00:00
+#SBATCH --partition=posa-compute
 
 source /scratch/hien/venvs/rl_contacts_env/bin/activate
 cd /scratch/hien/rl-baselines3-zoo
@@ -19,11 +19,12 @@ TUNING_NO_STEP=200000
 TUNING_NO_TRIALS=1000
 TUNING_SAMPLER=tpe
 TUNING_PRUNER=median
-TUNING_PATH=/scratch/hien/rl-baselines3-zoo/logs/$ALGO
 
 if $TUNING
 then
-    python train.py --algo $ALGO --env $ENV -n $TUNING_NO_STEP -optimize --n-trials $TUNING_NO_TRIALS --sampler $TUNING_SAMPLER --pruner $TUNING_PRUNER --optimization-log-path $TUNING_PATH
+    python train.py --algo $ALGO --env $ENV -n $TUNING_NO_STEP -optimize --n-trials $TUNING_NO_TRIALS --sampler $TUNING_SAMPLER --pruner $TUNING_PRUNER
 else
     python train.py --algo $ALGO --env $ENV -n $TUNING_NO_STEP --tensorboard-log $TENSORBOARD_LOG_DIR
 fi
+
+exit 0
